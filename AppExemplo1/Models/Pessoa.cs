da069a27acc60393c0c0e31f342bbce647fd41e8
2019102049035-5 <2019102049035-5@JIPA-LABIN03-28>
2024-02-20 21:50:01 -0400

    Adicionar arquivos de projeto.

---
+++ AppExemplo1/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace AppExemplo1.Models
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public double CalcularImc()
+        {
+            return Peso / (Altura * Altura);
+        }
+    }
+}
